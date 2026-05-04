bfcc4f546a4767f84b4c610db7b560774e4b2f6c
m1haww <ozunmihai5@gmail.com>
2026-05-04 20:23:57 +0300

    adaugat helpers sleep range unique in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function range(start: number, end: number, step = 1): number[] {
+  const result: number[] = []
+  for (let i = start; i < end; i += step) result.push(i)
+  return result
+}
+
+export function unique<T>(arr: T[]): T[] {
+  return Array.from(new Set(arr))
+}
